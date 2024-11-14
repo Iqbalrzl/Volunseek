3dd1c661edd401d3fcf50898402b4cf9ece35de5
Iqbaal <iqbaalrizaaldy1@gmail.com>
2024-11-14 22:04:06 +0700

    Create Admin Page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,42 @@
+from datetime import date
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'event_type', 'description',
+                    'start_date', 'end_date', 'location', 'status']
+    list_editable = ['event_type']
+    list_select_related = ['event_type']
+
+    def status(self, event):
+        if date.today() > event.end_date:
+            return 'Completed'
+        elif date.today() < event.start_date:
+            return 'Not Started'
+        return 'Ongoing'
+
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ['type']
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'event', 'participant', 'enrollment_date']
+    list_select_related = ['participant', 'event']
+
+    def participant(self, enrollment):
+        return enrollment.participant.username
+
+    def event(self, enrollment):
+        return enrollment.event.title
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,24 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+
 class EventType(models.Model):
     type = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        ordering = ['type']
+
+
 class Event(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
     start_date = models.DateField()
     end_date = models.DateField()
     location = models.CharField(max_length=255)
-    event_type = models.ForeignKey(EventType, on_delete= models.SET_NULL, null=True)
+    event_type = models.ForeignKey(
+        EventType, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class Participant(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=8)
     email = models.EmailField(unique=True)
 
+    def __str__(self):
+        return self.username
+
+
 class Enrollment(models.Model):
-    participant = models.ForeignKey(Participant,on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     enrollment_date = models.DateField(auto_now_add=True)
-    
