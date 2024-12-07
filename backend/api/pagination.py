aaca46ddd64645577e94b82a8ea38474d5cde9dd
Iqbaal <iqbaalrizaaldy1@gmail.com>
2024-12-08 03:30:54 +0700

    Fix Pagination Query Params

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class EventPagination(PageNumberPagination):
     page_size = 12
-    page_query_param = 'page_size'
+    page_query_param = 'page'
     max_page_size = 100
