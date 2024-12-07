4cd230eb544eb43a0cf618b5620177154d741377
Iqbaal <iqbaalrizaaldy1@gmail.com>
2024-12-08 02:39:56 +0700

    Fix Page Size in EventPagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class EventPagination(PageNumberPagination):
-    page_size = 16
+    page_size = 12
     page_query_param = 'page_size'
     max_page_size = 100
